4128a9e57b9f1395a93ae8e426aa191816265a70
Richterman <richterman@protonmail.com>
2023-07-13 23:04:53 -0500

    Add files via upload
    latest update that i wrote on my pi at work july 13th 2300 hours

--- jak.sh
+++ jak.sh
@@ -6,86 +6,84 @@
 echo
 echo
 distro=unkown
-
-Git_install() {
-if ( cd ~/Games/jak-project )
-then
-	if ( cd ~/Games/jak-project/iso_data/jak1 )
+install_location=~/Games/jak-project/		## will update and make user setable later on, not focused on it
+version=ntscv1
+git_run() {
+	distrocheck
+		cd ~/Games
+		git clone https://github.com/open-goal/jak-project
+		cd jak-project/
+		if [ $distro = redhat ]
+			then
+				cmake -DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=lld" -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -B build
+				cmake --build build -j$(nproc)
+		else
+			cmake -B build && cmake --build build -j 8
+		fi
+		
+	./test.sh ; echo ; echo "Succesfully installed git and tested"
+}
+Upate() {
+	jak1=0
+	jak2=0
+	if [ cd $install_location ]
+	then 
+		cd ~/
+		if [ cd $install_location/iso_data/jak1 ]
+		then
+			mv $install_location/iso_data/jak1 ~/
+			jak1=1
+		else 
+			echo
+		fi
+		if [ cd $install_location/iso_data/jak2 ]
+		then
+			mv $install_locaiton/iso_data/jak2 ~/
+			jak2=1
+		else 
+			echo
+		fi
+		rm -rf $install_location
+		distrocheck
+		cd ~/Games
+		git_run	
+	if [ $jak1 = 1 ]
 	then
-		cp ~/Games/jak-project/iso_data/jak1 ~/Documents
+		mv ~/jak1 $install_location/iso_data
+	elif
+		echo
 	fi
-	if ( cd ~/Games/jak-project/iso_data/jak2 )
+	if [ $jak2 = 1 ]
 	then
-		cp ~/Games/jak-project/iso_data/jak2 ~/Documents
+		mv ~/jak2 $install_location/iso_data/
+	elif
+		echo
 	fi
-	cd ~/Games/ ; git clone https://github.com/open-goal/jak-project
-	Distrocheck
-	cd jak-project/
-	if [[ $distro = debian || $distro = arch ]]
+	if [ $jak1 = 1 ]
 	then
-	cmake -B build && cmake --build build -j 8
-	./test.sh
+		python ./scripts/tasks/update-env.py --game jak1
+		python ./scripts/tasks/update-env.py --decomp_config $version
+	{{.DECOMP_BIN_RELEASE_DIR}}/decompiler ; ./decompiler/config/{{.DECOMP_CONFIG}} ; ./iso_data ; ./decompiler_out ; --version ; {{.DECOMP_CONFIG_VERSION}} ;  --config-override ; {\"decompile_code\": false, \"levels_extract\": true, \"allowed_objects\": []}'"	
+		./goalc --cmd "(mi)"
+		clear ; echo "jak1 installed"
 	else
-	cmake -DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=lld" -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -B build
-	cmake --build build -j$(nproc)
-	./test.sh
+		echo ; echo "jak1 not installed "
 	fi
-	cp ~/Documents/jak1/ ~/Games/jak-project/iso_data/ ; cp ~/Documents/jak2/ ~/Games/jak-project/iso_data/
-elif ( cd ~/Games )
-then
-	cd ~/Games/ ; git clone https://github.com/open-goal/jak-project
-	Distrocheck
-	cd jak-project/
-	if ( $distro = debian || $distro = arch )
+	if [ $jak2 = 2 ]
 	then
-	cmake -B build && cmake --build build -j 8
-	./test.sh
+		python ./scripts/tasks/update-env.py --game jak2
+		python ./scripts/tasks/update-env.py --decomp_config $version
+		"{{.DECOMP_BIN_RELEASE_DIR}}/decompiler \"./decompiler/config/{{.DECOMP_CONFIG}}\" \"./iso_data\" \"./decompiler_out\" --version \"{{.DECOMP_CONFIG_VERSION}}\" --config-override '{\"decompile_code\": false, \"levels_extract\": true, \"allowed_objects\": []}'"
+		./goalc --cmd "(mi)"
+		clear ; echo "Jak2 installed"
 	else
-	cmake -DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=lld" -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -B build
-	cmake --build build -j$(nproc)
-	./test.sh
+		echo ; echo "Jak2 not installed"
 	fi
-
-	cp ~/Documents/jak1/ ~/Games/jak-project/iso_data/ ; cp ~/Documents/jak2/ ~/Games/jak-project/iso_data/
-	echo ; echo "Jak1 folder and Jak2 folder copied from ~/Documents"
-else
-mkdir ~/Games
-cd ~/Games/ ; git clone https://github.com/open-goal/jak-project
-	Distrocheck
-	cd jak-project/
-	if [ $distro = debian || $distro = arch ]
-	then
-	cmake -B build && cmake --build build -j 8
-	./test.sh
-	else
-	cmake -DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=lld" -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -B build
-	cmake --build build -j$(nproc)
-	./test.sh
-	fi
-
-	cp ~/Documents/jak1/ ~/Games/jak-project/iso_data/ ; cp ~/Documents/jak2/ ~/Games/jak-project/iso_data/
-	echo ; echo "Jak1 folder and Jak2 folder copied from ~/Documents"
-fi
-
+	else 	
+	echo ; echo "Games not installed, please run -install option"
+	fi	
 }
 
-Distrocheck() {
-	local distro
-	if ( which apt )
-	then
-		distro=debian
-	elif ( which rpm )
-		distro=redhat
-	then
-		distro=redhat
-	elif ( which pacman )
-	then
-		distro=arch
-	else
-		echo ; echo "Error!! Unable to identify your package manager to determine distro base."
-		exit
-	fi
-}
 Depency_install() {
 local distro
 case $distro in
@@ -105,92 +103,67 @@ esac
 echo ; while [ -n "$1" ]
 do
 case "$1" in
-	-1)						### play jak 1
-		cd ~/Games/jak-project
-		if [ $distro != arch ]
+	-jak1)						### play jak 1
+		if [ cd $insatll_location/jak1 ]
 		then
-			task set-game-jak1
-			task boot-game-retail
-		else 
-			go-task set-game-jak1
-			go-task boot-game-retail
+			cd $install_location
+			echo ; echo "Booting jak in retail"
+			python ./scripts/tasks/update-env.py --game jak1
+			{{.GK_BIN_RELEASE_DIR}}/gk -v --game {{.GAME}} -- -boot -fakeiso
+			cd
+		else
+			echo ; echo "Jak1 not installed"
 		fi;;
-	-2)						### play jak 2
-		cd ~/Games/jak-project
-		if [ $distro != arch ]
+
+	-jak1debug)
+		if [ cd $install_location/jak1 ]
 		then
-			echo ; echo "Warning!! Jak2 can only be played in debug mode" ; task set-game-jak2
-			task boot-game
-		else 
-			echo ; echo "Warning!! Jak2 can only be played in debug mode" ; go-task set-game-jak2
-			go-task boot-game
+			cd $install_location
+			echo ; echo "Booting jak1 in debug"
+			python ./scripts/tasks/update-env.py --game jak1
+			{{.GK_BIN_RELEASE_DIR}}/gk -v --game {{.GAME}} -- -boot -fakeiso -debug
+			cd
+		else
+			echo ; echo "Jak1 not installed"
 		fi;;
-	-h)			### help
-		echo ; echo "-1 = play jak1, -2 = play jak2, -jak1i = install jak1, jak2i = install jak2, build = install jak 1 and jak 2"
-		echo ; echo "please press -h to show this again"
-		echo ; echo "run the depencdy check to add this script to /usr/local/bin so you can run directly from cmd line"
+			
+	-jak2)		### play jak2 in retail
+		if [ cd $install_location/iso_data/jak2 ]
+		then
+			cd $install_location
+			echo ; echo "Booting in retail"
+			{{.GK_BIN_RELEASE_DIR}}/GK -v --game {{.GAME}} -- -boot -fakeiso
+		else 
+			echo ; echo "Jak2 not installed"
+		fi;;
+	-jak2debug)	### play jak 2 in debug
+		if [ cd $install_location/jak2 ]
+		then
+			cd $install_location
+			echo ; echo "Booting in debug"
+			python ./scripts/tasks/update-env.py --game jak2
+			{{.GK_BIN_RELEASE_DIR}}/gk -v --game {{.GAME}} -- -boot -fakeiso -debug
+			cd
+			break
+		else 
+			echo ; echo "Jak2 not installed"
+		fi;;		
+	
+	-h || -help)			### help
+		echo ; echo -e "\t\t\tcommand list"
+		echo ; echo -e "\tjak - jak1 \t = play jak1 in retail"
+		echo ; echo -e "\tjak -jak2 \t = play jak2 in retail"
+		echo ; echo -e "\t\tAdd jak1debug or jak2debug launches in debug mode"
+		echo ; echo -e "\tjak -install = install the game from source"
+		echo ; echo -e "\tjak -update = updates the games from source"
+		echo ; echo -e "\tjak -h shows this menu"
 		break;;
-	-d) 				## install depencies
-	if [ whoami = root ]
-	then
-			Distrocheck
-			Depency_install
-			cp $( pwd ) /usr/local/bin
-			mv /usr/local/bin/jak.sh /usr/local/bin/jak
-	else
-		echo ; echo "Error!! Please run this script as root to install depencies"
-		exit
-		fi;;
+		
+	-install)
+		Depency_install
 
-	-build)									## build both games
-	echo ; echo "This command will also rebuild from source"
-			Git_install
-			cd ~/Games/jak-project
-			if [ $distro != arch ]
-			then
-				task set-game-jak1 ; task set-decomp-ntscv1 ; task extract
-				echo ; echo "build game"
-				./goalc --cmd "(mi)"
-			else
-				go-task set-game-jak1 ; go-task set-decomp-ntscv1 ; go-task extract
-				./goalc --cmd "(mi)"
-				fi
-				cd ~/Games/jak-project/
-			if [ $distro != arch ]
-			then
-				task set-game-jak2 ; task set-decomp-ntscv1 ; task extract
-				./goalc --cmd "(mi)"
-				exit
-			else
-				go-task set-game-jak2 ; go-task set-decomp-ntscv1 ; go-task extract
-				./goalc --cmd "(mi)"
-				exit
-				fi ;;
 
-	-jak1i)				## install jak1 and source
-			echo ; echo "This command will also rebuild from source"
-			Git_install
-			cd ~/Games/jak-project
-			if [ $distro != arch ]
-			then
-				task set-game-jak1 ; task set-decomp-ntscv1 ; task extract
-				./goalc --cmd "(mi)"
-			else
-				go-task set-game-jak1 ; go-task set-decomp-ntscv1 ; go-task extract
-				./goalc --cmd "(mi)"
-				fi ;;
-	-jak2i)					## install jak2 and source
-					echo ; echo "This command will also rebuild from source"
-			Git_install
-			cd ~/Games/jak-project/
-			if [ $distro != arch ]
-			then
-				task set-game-jak2 ; task set-decomp-ntscv1 ; task extract
-				task repl
-			else
-				go-task set-game-jak2 ; go-task set-decomp-ntscv1 ; go-task extract
-				go-task replace
-				fi ;;
+	
 
 	*)	echo ; echo "That's not a valid option, please add -h for help";;
 #shift
